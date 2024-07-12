2a10c0e381ab224fcae009081293b690e8968f60
Kecerim24 <kolouch.mirda@gmail.com>
2024-07-12 13:22:38 +0200

    fair queue revert to upstream

--- src/main/java/com/jagrosh/jmusicbot/queue/FairQueue.java
+++ src/main/java/com/jagrosh/jmusicbot/queue/FairQueue.java
@@ -25,129 +25,30 @@ import java.util.Set;
  */
 public class FairQueue<T extends Queueable> extends AbstractQueue<T>
 {
-    public FairQueue(AbstractQueue<T> queue)
-    {
-        super(queue);
-    }
+	public FairQueue(AbstractQueue<T> queue)
+	{
+		super(queue);
+	}
 
-    protected final Set<Long> set = new HashSet<>();
+	protected final Set<Long> set = new HashSet<>();
 
-    @Override
-    public int add(T item)
-    {
-        int lastIndex;
-        for(lastIndex=list.size()-1; lastIndex>-1; lastIndex--)
-            if(list.get(lastIndex).getIdentifier() == item.getIdentifier())
-                break;
-        lastIndex++;
-        set.clear();
-        for(; lastIndex<list.size(); lastIndex++)
-        {
-            if(set.contains(list.get(lastIndex).getIdentifier()))
-                break;
-            set.add(list.get(lastIndex).getIdentifier());
-        }
-        list.add(lastIndex, item);
-        return lastIndex;
-    }
+	@Override
+	public int add(T item)
+	{
+		int lastIndex;
+		for(lastIndex=list.size()-1; lastIndex>-1; lastIndex--)
+			if(list.get(lastIndex).getIdentifier() == item.getIdentifier())
+				break;
+		lastIndex++;
+		set.clear();
+		for(; lastIndex<list.size(); lastIndex++)
+		{
+			if(set.contains(list.get(lastIndex).getIdentifier()))
+				break;
+			set.add(list.get(lastIndex).getIdentifier());
+		}
+		list.add(lastIndex, item);
+		return lastIndex;
+	}
 
-    public int betterADD(T item){
-        int lastIndex;
-        for(lastIndex=list.size()-1; lastIndex>-1; lastIndex--)
-            if(list.get(lastIndex).getIdentifier()==item.getIdentifier())
-                break;
-        lastIndex++;
-        set.clear();
-        for(; lastIndex<list.size(); lastIndex++)
-        {
-            if(set.contains(list.get(lastIndex).getIdentifier()))
-                break;
-            set.add(list.get(lastIndex).getIdentifier());
-        }
-
-        list.add(item);
-
-        return lastIndex;
-
-    }
-
-    public void addAt(int index, T item)
-    {
-        if(index >= list.size())
-            list.add(item);
-        else
-            list.add(index, item);
-    }
-
-    public int size()
-    {
-        return list.size();
-    }
-
-    public T pull()
-    {
-        return list.remove(0);
-    }
-
-    public boolean isEmpty()
-    {
-        return list.isEmpty();
-    }
-
-    public T get(int index)
-    {
-        return list.get(index);
-    }
-
-    public T remove(int index)
-    {
-        return list.remove(index);
-    }
-
-    public int removeAll(long identifier)
-    {
-        int count = 0;
-        for(int i=list.size()-1; i>=0; i--)
-        {
-            if(list.get(i).getIdentifier()==identifier)
-            {
-                list.remove(i);
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public void clear()
-    {
-        list.clear();
-    }
-
-    public void unskip(T item){
-        list.add(0, item);
-    }
-
-    public void skip(int number)
-    {
-        for(int i=0; i<number; i++)
-            list.remove(0);
-    }
-
-    /**
-     * Move an item to a different position in the list
-     * @param from The position of the item
-     * @param to The new position of the item
-     * @return the moved item
-     */
-    public T moveItem(int from, int to)
-    {
-        T item = list.remove(from);
-        list.add(to, item);
-        return item;
-    }
-
-    @Override
-    public String toString() {
-        return "list=" + list;
-    }
 }
